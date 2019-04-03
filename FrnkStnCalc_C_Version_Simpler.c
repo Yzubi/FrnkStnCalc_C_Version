24d3c4e9ed9fa5eafa5ab3cf39e7b0928067d1f0
Yzubi <42348748+Yzubi@users.noreply.github.com>
2019-04-03 15:57:31 -0300

    Add files via upload

---
+++ FrnkStnCalc_C_Version_Simpler.c
@@ -0,0 +1,75 @@
+/*=== This is FrnkStnCalc, a simple command line calculator, C version ===*/
+
+/* Include list */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Declare variables */
+double Argument1, Argument3, DataResults;
+char Argument2, *PrintStringHello = "Hello! :)";
+
+/* Declare functions */
+void FuncResults(double DataResults);
+
+/* Start program, main function */
+int main(int argc, char *argv[])
+{
+	/* || means OR in C, Argc is a function, and the name means "argument count" */
+	if (argc > 1 + 3 || argc < 1 + 3)
+	{
+		printf("The calculator is limited to 2 numbers operations...\n");
+		/* Exit program */
+		return 0;
+	}
+
+	/* === Arguments === */
+	/* Make heyhey = argv[2] */
+	char *heyhey = argv[2];
+	
+	/* Convert string to float using Atoi */
+	Argument1 = atoi(argv[1]); /*Arg1*/
+	Argument2 = argv[2][0];  /*Arg2*/
+	Argument3 = atoi(argv[3]); /*Arg3*/
+
+	/*==== Operator conditions and DataResults print function ===*/
+	/* Plus */
+	if (Argument2 == '+')
+	{
+		FuncResults(Argument1 + Argument3);
+	}
+	/* Minus */
+	else if (Argument2 == '-')
+	{
+		FuncResults(Argument1 - Argument3);
+	}
+	/* Division */
+	else if (Argument2 == '/')
+	{
+		FuncResults(Argument1 / Argument3);
+	}
+	/* Multiply */
+	else if (Argument2 == 'x')
+	{
+		FuncResults(Argument1 * Argument3);
+	}
+	/* Say hello if "hey" input is received as second argument */ /* Compare strings using strncmp /* /* You can also use strcmp if you want to compare the full strings without a character limit (See first FrnkStnCalc ver) */
+	else if (strcmp(heyhey, "hey") == 0)
+	{
+		printf("%s\n", PrintStringHello);
+	}
+	/* Print an error message if a inexistent operator is used */
+	else
+	{
+		printf("Operator does not exist\n");
+	}
+	/* Exit program */
+	return 0;
+}
+
+/* === Functions === */
+/* Print DataResults function */
+void FuncResults(double DataResults)
+{
+	printf("Result: %f %c %f = %f\n", Argument1, Argument2, Argument3, DataResults);
+}
